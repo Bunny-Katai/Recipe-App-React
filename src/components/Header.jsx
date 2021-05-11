724f94523cda3ab3a1e24160ab15b3675b0fd386
Supatchaya <tai.pakdee@gmail.com>
2021-05-11 09:25:38 -0600

    fixing navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,38 @@
 import { useContext } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import logo from './Logo.png';
 
 
 export default function Header ({ location }) {
    
 
     return (
-      <header>
-        <Menu>
-            <Menu.Item
-                name='home'
-               
-            >
-                <Link to="/home">Home</Link>
-            </Menu.Item>
-        
-            <>
-                <Menu.Item
-                name='recipe'
-              
-                >
-                <Link to="/recipe">Recipes</Link>
-                </Menu.Item>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to='/home' className="navbar-brand">
+            <img className="Logo" src= { logo } alt="logo" width='180'/>
+        </Link>
+        <div className="collapse navbar-collapse show ml-sm-5">
+            <ul className="navbar-nav">
+                <li className="navbar-item">
+                    <Link className="nav-link" to='/home'>
+                        Home 
+                    </Link>
+                </li>
 
-                <Menu.Item
-                name='favorite'
-              
-                >
-                 <Link to="/favorite">Favorite</Link>
-                </Menu.Item>
-            </>
-          
-            <Menu.Item
-               
-                name='gallery'
-            >
-                <Link to="/gallery">Gallery</Link>
-            </Menu.Item>
-        
-           
-        </Menu>
-    </header>
+                <li className="navbar-item">
+                    <Link className="nav-link" to='/recipes'>
+                        Recipes
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link className="nav-link" to='/favorite'>
+                        Favorite
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
     );
 }
+
