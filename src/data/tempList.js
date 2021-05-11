69d382ca12ecd389b22b0de30da255c702bac739
Supatchaya <tai.pakdee@gmail.com>
2021-05-11 09:28:12 -0600

    adding temperary data

---
+++ src/data/tempList.js
@@ -0,0 +1,28 @@
+export const recipeData = [
+    {
+        label:"Thai Pineapple Curry",
+        url: "https://hot-thai-kitchen.com/pineapple-curry/",
+    
+        image: 
+        "https://hot-thai-kitchen.com/wp-content/uploads/2020/09/THAI-PINEAPPLE-SHRIMP-CURRY-683x1024.jpg"
+    },
+
+
+    {
+        label:"Kao Moo Dang",
+        url: "https://hot-thai-kitchen.com/kao-moo-dang/",
+       
+        image: 
+        "https://hot-thai-kitchen.com/wp-content/uploads/2021/03/Kao-moo-dang.jpg"
+    },
+
+    {
+        label:"Sweet Rice & Mango",
+        url: "https://hot-thai-kitchen.com/mango-sticky-rice/",
+        
+        image: 
+        "https://hot-thai-kitchen.com/wp-content/uploads/2014/02/Mango-Sticky-225x225.jpg"
+    },
+
+
+]
