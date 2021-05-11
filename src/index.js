d4a7c78b91773e17911d9dc2f27eb6c792a38bae
Supatchaya <tai.pakdee@gmail.com>
2021-05-11 13:54:28 -0600

    Adding global state

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter} from 'react-router-dom'
+import { GlobalContextProvider} from './data/GlobalContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <GlobalContextProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+
+  </GlobalContextProvider>,
   document.getElementById('root')
 );
 
