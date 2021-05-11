dc8942de83d2afd810eba1bf1cb4c40bf22522d3
Supatchaya <tai.pakdee@gmail.com>
2021-05-11 09:27:43 -0600

    Adding recipecard component

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,28 @@
+import Reac from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Icon, Image, Button} from 'semantic-ui-react'
+
+const RecipeCard =({title,image, url}) => {
+    return (
+     
+      <div className='RecipeContainer'>
+       <Card>
+            <Image src={image} alt="recipe card" size='medium' />
+          <Card.Content>
+            <Card.Header>{title}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='like' />
+            </a>
+            <Button floated='right'><Link to='recipes' className='RecipeButton'>Recipe</Link></Button>
+          </Card.Content>
+      </Card>
+      </div>
+        
+    );
+  };
+
+export default RecipeCard
+
+
