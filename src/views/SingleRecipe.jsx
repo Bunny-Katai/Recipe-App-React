72817879c08168ec78c451231841808e064c99a6
Supatchaya <tai.pakdee@gmail.com>
2021-05-11 13:59:03 -0600

    Adding funciton to call uri

--- src/views/SingleRecipe.jsx
+++ src/views/SingleRecipe.jsx
@@ -1,25 +1,29 @@
 import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 export default function SingleRecipe() {
     const APP_ID = "7eb16d6b";
     const APP_KEY= "b0591ba0693bd241bd0db73d50b41ae8";
-    
+    const {uri}=useParams();
+    const [recipe, setRecipe] = useState({});
     useEffect ( () => { 
         getSpecificRecipe();
-      }, []); 
+      }, [uri]); 
     
       const getSpecificRecipe = async () => {
-        const response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9b5945e03f05acbf9d69625138385408`)
-    
+        const stringUri = atob(uri);
+        console.log('stringUri', stringUri);
+        const response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&r=${stringUri}`);
+        
         const data = await response.json();
+        setRecipe(data)
         console.log(data)
       }
 
     return (
         <div>
-            
+         <pre>{JSON.stringify(recipe, null, 2)}</pre>   
         </div>
     )
       
