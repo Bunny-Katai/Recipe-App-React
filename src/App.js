c99f79dc82d7286b7ccfba4e2efa6fd39ec4e67c
Supatchaya <tai.pakdee@gmail.com>
2021-05-11 09:25:09 -0600

    Adding route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import Reac, { useEffect } from 'react';
 import './App.css';
 import Home from './views/Home';
-import { Route, Switch, Link  } from 'react-router-dom';
+import { Route, Switch, Link , Router } from 'react-router-dom';
 import Recipe from './views/Recipes';
 import Favorite from './views/Favorite';
+import SingleRecipe from './views/SingleRecipe';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Recipes from './views/Recipes';
+
 
 
 
 
 function App() {
   
+
   return (
     <>
       <Header /> 
       <Switch>
-        <Route path='/home'>
+        <Route exact path='/home'>
           <Home />
         </Route>
-        <Route path='/recipe'>
-          <Recipe />
+        <Route exact path='/recipes'>
+          <Recipes />
+        </Route>
+        <Route path='/recipes/:id'>{ SingleRecipe }
         </Route>
         <Route path='/favorite'>
           <Favorite />
