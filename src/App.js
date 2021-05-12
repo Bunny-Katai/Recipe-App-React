18897b2771c88d65b479ee29a19d6a23ab790f93
Supatchaya <tai.pakdee@gmail.com>
2021-05-12 08:18:10 -0600

    import boostrap

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Recipes from './views/Recipes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
